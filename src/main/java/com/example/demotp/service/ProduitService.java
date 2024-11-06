9f9a87660276fbeef621f453fe326cc7d65179c1
hanaabid14 <hanaabid2021@gmail.com>
2024-11-06 14:57:18 +0100

    design by gimme

--- src/main/java/com/example/demotp/service/ProduitService.java
+++ src/main/java/com/example/demotp/service/ProduitService.java
@@ -1,31 +1,34 @@
 package com.example.demotp.service;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demotp.model.Produit;
 import com.example.demotp.repository.ProduitRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class ProduitService {
- @Autowired
- private ProduitRepository produitRepository;
- public List<Produit> findAll() {
- return produitRepository.findAll();
- }
- 
-public Produit save(Produit produit) {
-    return produitRepository.save(produit);
+
+    @Autowired
+    private ProduitRepository produitRepository;
+
+    public List<Produit> findAll() {
+        return produitRepository.findAll();
     }
+
+    public Produit save(Produit produit) {
+        return produitRepository.save(produit);
+    }
+
     public Produit findById(Long id) {
-    Optional<Produit> produit = produitRepository.findById(id);
-    return produit.orElse(null);
+        Optional<Produit> produit = produitRepository.findById(id);
+        return produit.orElse(null);
     }
+
     public void delete(Long id) {
-     produitRepository.deleteById(id);
- } 
- //test nour
-  
+        produitRepository.deleteById(id);
+    }
 }
